75d45f8372e087cd988a66434698d046df1bc279
Fardeen Panjwani <fardeen.panjwani@vxlllc.com>
2020-01-17 00:27:39 +0530

    created Document class(lots of TODOs)

---
+++ src/agents/document/document.c.ts
@@ -0,0 +1,32 @@
+import Timestamp from "../common/timestamp.i";
+import Document from "./document.i";
+
+export default class DocumentClass implements Document {
+    public static create(name: string) {
+        // generate an id
+        // create a document object
+        // push it to firestore
+    }
+
+    public static fetchAll(parent: string) {
+        // fetch parent's all documents from firestore
+    }
+
+    id: string;
+    name: string;
+    created: Timestamp;
+    lastUpdated: Timestamp;
+
+    constructor(documentID: string) {
+        this.id = documentID;
+    }
+
+    public get() {
+        // get document from firestore and return it
+    }
+
+    public updateName(newName: string) {
+        this.name = newName;
+        // update the document in firestore using this.id
+    }
+}
