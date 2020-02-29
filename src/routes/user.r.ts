18d98c144da3f7e4ecb04a8913dbcedd9f44efc6
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-02-29 12:08:49 +0530

    added error logger to user's login route

--- src/routes/user.r.ts
+++ src/routes/user.r.ts
@@ -93,6 +93,7 @@ router.post(`${login}`, (req: Request, res: Response) => {
             }).end();
         })
         .catch((err) => {
+            console.log(err.message);
             return res.status(500).json({
                 error: err.message
             }).end();
